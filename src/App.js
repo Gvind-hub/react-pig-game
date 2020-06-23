997e16b420e5535ebc173514450258008979982c
Gvind-hub <63408287+Gvind-hub@users.noreply.github.com>
2020-06-23 23:32:38 +0300

    finished layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import dice1 from './assets/img/dice-1.png';
+import Card from "./components/Player/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <body>
+        <div className="game">
+            <button className="game__btn-new">
+                <ion-icon name="add-circle-outline"/>
+                <span>new game</span>
+            </button>
+            <div className="game__left">
+                <Card/>
+            </div>
+            <div className="game__right">
+              <Card/>
+            </div>
+            <img src={dice1} alt="dice" className="game__dice-1"/>
+            <img src={dice1} alt="dice" className="game__dice-2"/>
+            <button className="game__btn-roll">
+                <ion-icon name="reload-outline"/>
+                <span>roll dice</span>
+            </button>
+            <button className="game__btn-hold">
+                <ion-icon name="download-outline"/>
+                <span>hold</span>
+            </button>
+            <input type="number" className="game__top-score"/>
+        </div>
+        </body>
+    );
 }
 
 export default App;
