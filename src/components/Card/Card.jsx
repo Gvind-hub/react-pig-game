9aadd64b6db290817aa1a1e20b56bdb5de8a2920
Gvind-hub <63408287+Gvind-hub@users.noreply.github.com>
2020-06-24 21:19:35 +0300

    player's name input decorated

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,20 +6,24 @@ import './Card.scss';
 const Card = ({currentScore, score, active, playerName, setPlayerName}) => {
 
     const [toggleInput, setToggleInput] = useState(false);
+
     const changeName = e => {
         setPlayerName(e.target.value)
     };
     const inputSubmit = () => {
-        if (playerName && playerName !== 'enter player\'s name') {setToggleInput(false)}
-        else {
-            setPlayerName('enter player\'s name');
+        const noName = 'enter player\'s name'
+        if (playerName && playerName !== noName) {
+            setToggleInput(false)
+        } else {
+            setPlayerName(noName);
         }
     };
     return (
         <div className="card">
-            {toggleInput && <input type="text" minLength="1" maxLength="10" onChange={changeName}
-                                   value={playerName} onBlur={inputSubmit} onFocus
-                                   onKeyPress={e => e.key === "Enter" && inputSubmit()}/>}
+            {toggleInput && <input className="card__name-input"
+                type="text" minLength="1" maxLength="10" onChange={changeName}
+                value={playerName} onBlur={inputSubmit} autoFocus
+                onKeyPress={e => e.key === "Enter" && inputSubmit()}/>}
             {!toggleInput && <h2 className={cn("card__player-name", {"active": active})}
                                  onDoubleClick={() => setToggleInput(true)}>{playerName}</h2>}
             <p className="card__player-score">{score}</p>
